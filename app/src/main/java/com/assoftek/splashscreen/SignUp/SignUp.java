48ccb6ebe000086f8d0eafe6fc850dad669d564e
prashantrohilla <prashantrohilla32@gmail.com>
2021-05-04 22:28:42 +0530

    google  sign up done by anand

--- app/src/main/java/com/assoftek/splashscreen/SignUp/SignUp.java
+++ app/src/main/java/com/assoftek/splashscreen/SignUp/SignUp.java
@@ -10,11 +10,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.assoftek.splashscreen.R;
 import com.assoftek.splashscreen.databinding.ActivitySignupBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -23,6 +36,11 @@ import java.util.concurrent.TimeUnit;
 public class SignUp extends AppCompatActivity {
 
     ActivitySignupBinding binding;
+    GoogleSignInClient googleSignInClient;
+    FirebaseAuth mAuth;
+    private int RC_SIGN_IN=1;
+
+
 
     Button signup;
 
@@ -32,6 +50,23 @@ public class SignUp extends AppCompatActivity {
         binding=ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mAuth=FirebaseAuth.getInstance();
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions);
+
+        binding.google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.progressBar.setVisibility(View.VISIBLE);
+                binding.signUpButton.setVisibility(View.GONE);
+                signupwithgoogle();
+            }
+        });
 
         binding.signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,4 +143,67 @@ public class SignUp extends AppCompatActivity {
         });
     }
 
+    private void signupwithgoogle() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handlesignInResult(task);
+        }
+    }
+
+    private void handlesignInResult(Task<GoogleSignInAccount> completedtask) {
+
+        try {
+            GoogleSignInAccount acc = completedtask.getResult(ApiException.class);
+            Toast.makeText(this, "Signned In Successfully", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(acc);
+        }catch (ApiException e){
+            Toast.makeText(this, "Signned In Failed", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void FirebaseGoogleAuth(GoogleSignInAccount acc) {
+
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(acc.getIdToken(),null);
+        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+
+
+
+                    Toast.makeText(SignUp.this, "SuccessFull", Toast.LENGTH_SHORT).show();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateuser(user);
+                    binding.progressBar.setVisibility(View.GONE);
+                    binding.signUpButton.setVisibility(View.VISIBLE);
+                }else {
+                    binding.progressBar.setVisibility(View.GONE);
+                    binding.signUpButton.setVisibility(View.VISIBLE);
+                    Toast.makeText(SignUp.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void updateuser(FirebaseUser fuser) {
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if (account!=null)
+        {
+
+            String person_email = account.getEmail();
+
+            binding.email.setText(person_email);
+
+        }
+
+    }
 }
