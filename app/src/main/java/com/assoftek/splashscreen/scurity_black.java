e91d3789f007983ddac7e0622bf9c5821a9b47fb
swathinarayana <swathisrinarayana@gmail.com>
2021-06-11 19:55:29 +0530

    securityscreen

---
+++ app/src/main/java/com/assoftek/splashscreen/scurity_black.java
@@ -0,0 +1,13 @@
+package com.assoftek.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class scurity_black extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scurity_black);
+    }
+}
---
+++ app/src/main/java/com/assoftek/splashscreen/security_white.java
@@ -0,0 +1,14 @@
+package com.assoftek.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class security_white extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_white);
+    }
+}
