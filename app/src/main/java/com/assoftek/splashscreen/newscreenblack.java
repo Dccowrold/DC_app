43c21f0480f59195715fb531b60b7ee75adc231f
Naveena0607 <surakantinaveenareddy@gmail.com>
2021-06-11 18:42:00 +0530

    screen with chat

---
+++ app/src/main/java/com/assoftek/splashscreen/newscreenblack.java
@@ -0,0 +1,14 @@
+package com.assoftek.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newscreenblack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newscreenblack);
+    }
+}
