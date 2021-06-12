e6bcf6b9fb8cc64ca5ef066e7904c646f5abab6c
binitkumar200022 <binitkumar200022@gmail.com>
2021-06-12 15:18:41 +0530

    UserProfile correction in AppDatabase

--- app/src/main/java/com/assoftek/splashscreen/db/AppDatabase.java
+++ app/src/main/java/com/assoftek/splashscreen/db/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class, Details.class}, version = 1)
+@Database(entities = {User.class, Details.class, UserProfile.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract UserDao userDao();
