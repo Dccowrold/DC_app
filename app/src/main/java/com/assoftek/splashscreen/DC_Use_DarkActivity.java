2d9423504287c6b85d80eab03a76cebab047bc3f
Chintan1142 <chintan11402@gmail.com>
2021-06-19 15:21:26 +0530

    Modified code of Navigation drawer menu and modified some image also.

---
+++ app/src/main/java/com/assoftek/splashscreen/DC_Use_DarkActivity.java
@@ -0,0 +1,14 @@
+package com.assoftek.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DC_Use_DarkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dc_use_dark);
+    }
+}
